5b019206622f9848e90c436ba2584be9847cda84
wb-keeper <wb-keeper@mail.ru>
2023-10-18 19:36:44 +0300

    working on search

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -1,3 +1,4 @@
+'use client'
 import React, {useContext} from 'react';
 import Search from "@/app/components/Search";
 import {SearchContext} from "@/app/context/search";
@@ -5,11 +6,11 @@ import {SearchContext} from "@/app/context/search";
 function Hero(props) {
     const {searchActive} = useContext(SearchContext)
     return (
-        <section>
+        <section className="h-screen xl:h-[90vh] bg-[#b2b7c2]/10" id="home">
             {
                 searchActive ? (<div className="fixed top-[80px] z-10 w-full max-w-[1920px]">
                     <Search />
-                </div>) : (<div><Search /></div>)
+                </div>) : (<div className="-mt-12 w-full max-w-[1300px] mx-auto"><Search /></div>)
             }
             <Search /></section>
     );
