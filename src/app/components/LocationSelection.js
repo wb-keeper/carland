11adee980c4e054fb65b2ed48709d6ce0566b5f9
wb-keeper <wb-keeper@mail.ru>
2023-10-22 18:48:50 +0300

    working on location items

--- src/app/components/LocationSelection.js
+++ src/app/components/LocationSelection.js
@@ -28,7 +28,11 @@ function LocationSelection(props) {
             </div>
           </div>
         </Menu.Button>
-        <Menu.Items>Menu Items</Menu.Items>
+        <Menu.Items className="dropdown-menu shadow-lg absolute -top-56 xl:top-[90px] left-1/2 xl:left-0 z-10 transform -translate-x-1/2 xl:-translate-x-0 text-sm text-center xl:text-left w-full bg-white max-w-[332px] py-6 rounded-[10px]">
+          {locations.map((location, index) => {
+            return <div key={index}>{location}</div>;
+          })}
+        </Menu.Items>
       </div>
     </Menu>
   );
