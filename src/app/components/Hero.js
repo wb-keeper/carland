c46e9f29f7b52b42ff3c1aaacffc7776f334c650
wb-keeper <wb-keeper@mail.ru>
2023-10-24 19:34:49 +0300

    working on framer motion

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -3,6 +3,9 @@ import React, { useContext } from "react";
 import Search from "@/app/components/Search";
 import { SearchContext } from "@/app/context/search";
 import Image from "next/image";
+import { motion, easeInOut } from "framer-motion";
+import { fadeIn } from "variants";
+
 function Hero(props) {
   const { searchActive } = useContext(SearchContext);
   return (
@@ -10,18 +13,36 @@ function Hero(props) {
       <div className="container mx-auto h-full xl:pt-10">
         <div className="flex flex-col xl:flex-row justify-center items-center xl:justify-start h-full">
           <div className="text-center xl:max-w-xl xl:text-left mt-16 xl:mt-0">
-            <h1 className="h1">
+            <motion.h1
+              variants={fadeIn("down", 0.2)}
+              initial="hidden"
+              whileInView="show"
+              viewport={{ once: false, amount: 0.6 }}
+              className="h1"
+            >
               Explore the Finest <span className="text-accent">Global</span>{" "}
               Offers
-            </h1>
-            <p>
+            </motion.h1>
+            <motion.p
+              variants={fadeIn("down", 0.4)}
+              initial="hidden"
+              whileInView="show"
+              viewport={{ once: false, amount: 0.6 }}
+              className="description max-w-[550px] mx-auto xl:mx-0 mb-6 xl:mb-10"
+            >
               Find your ideal ride for any adventure with our diverse range of
               affordable and dependable car rentals.
-            </p>
-            <div className="flex gap-x-3 justify-center xl:justify-start">
+            </motion.p>
+            <motion.div
+              variants={fadeIn("down", 0.6)}
+              initial="hidden"
+              whileInView="show"
+              viewport={{ once: false, amount: 0.8 }}
+              className="flex gap-x-3 justify-center xl:justify-start"
+            >
               <button className="btn-cta">
                 <Image
-                  src={"/icons/buttons/google-play.svg"}
+                  src={"/icons/buttons/app-store.svg"}
                   width={132}
                   height={36}
                   alt="image"
@@ -35,19 +56,44 @@ function Hero(props) {
                   alt="image"
                 />
               </button>
-            </div>
+            </motion.div>
           </div>
-          <div>image</div>
+          <motion.div
+            variants={fadeIn("up", 0.6)}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: false, amount: 0.6 }}
+            className="relative w-full h-full max-w-[50vh] md:max-w-[70vh] xl:max-w-[860px] xl:max-h-[542px] xl:absolute xl:-right-[100px] min-[1680px]:right-[120px] xl:top-48"
+          >
+            <Image
+              src={"/images/hero/car.svg"}
+              fill
+              style={{ objectFit: "contain" }}
+              alt="car"
+              priority
+            />
+          </motion.div>
         </div>
       </div>
       {searchActive ? (
-        <div className="fixed top-[80px] z-10 w-full max-w-[1920px]">
+        <motion.div
+          initial={{ y: "-100%" }}
+          animate={{ y: 0 }}
+          transition={{ ease: easeInOut }}
+          className="fixed top-[80px] z-10 w-full max-w-[1920px]"
+        >
           <Search />
-        </div>
+        </motion.div>
       ) : (
-        <div className="-mt-12 w-full max-w-[1300px] mx-auto">
+        <motion.div
+          variants={fadeIn("up", 0.8)}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: false, amount: 0.2 }}
+          className="-mt-12 w-full max-w-[1300px] mx-auto"
+        >
           <Search />
-        </div>
+        </motion.div>
       )}
     </section>
   );
