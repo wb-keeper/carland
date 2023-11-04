000a44d822a7ad5e8be6d2a71ba9cac29d2019d5
wb-keeper <wb-keeper@mail.ru>
2023-11-04 18:35:46 +0300

    working on motion

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -5,7 +5,11 @@ import { useInView } from "react-intersection-observer";
 import { motion } from "framer-motion";
 import { fadeIn } from "/variants";
 import Image from "next/image";
-import { MdOutlineDirectionsCar, MdOutlineMapsHomeWork } from "react-icons/md";
+import {
+  MdOutlineBuildCircle,
+  MdOutlineDirectionsCar,
+  MdOutlineMapsHomeWork,
+} from "react-icons/md";
 
 const About = () => {
   const [ref, inView] = useInView({
@@ -19,7 +23,13 @@ const About = () => {
     >
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row xl:justify-between">
-          <div>
+          <motion.div
+            variants={fadeIn("up", 0.2)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.6 }}
+            className="flex-1 mb-8 xl:mb-0"
+          >
             <Image
               className="rounded-[20px]"
               src={"/images/about/car01.png"}
@@ -27,10 +37,12 @@ const About = () => {
               width={610}
               height={448}
             />
-          </div>
-          <div>
-            <div>
-              <h2>Car services simplified.</h2>
+          </motion.div>
+          <div className="flex-1 flex xl:justify-center items-center">
+            <div className="xl:max-w-[517px]">
+              <h2 className="h2 mb-[42px] max-w-md">
+                Car services simplified.
+              </h2>
               <p>
                 Rent, choose, and repair with ease. Out convenient locations,
                 diverse car types, and reliable repair points ensure a seamless
@@ -53,26 +65,27 @@ const About = () => {
                   <MdOutlineMapsHomeWork className="text-5xl text-accent mb22" />
                   <div className="text-5xl font-black mb-2">
                     {inView ? (
-                      <CountUp start={0} end={51} duration={3} delay={1} />
+                      <CountUp start={0} end={135} duration={3} delay={1} />
                     ) : null}
-                    +
                   </div>
                   <div className="uppercase text-[13px] font-semibold text-secondary">
-                    car <br /> types
+                    rental <br /> outlets
                   </div>
                 </div>
                 <div className="flex flex-col w-[100px]">
-                  <MdOutlineDirectionsCar className="text-5xl text-accent mb22" />
+                  <MdOutlineBuildCircle className="text-5xl text-accent mb22" />
                   <div className="text-5xl font-black mb-2">
                     {inView ? (
-                      <CountUp start={0} end={51} duration={3} delay={1} />
+                      <CountUp start={0} end={35} duration={3} delay={1} />
                     ) : null}
-                    +
                   </div>
                   <div className="uppercase text-[13px] font-semibold text-secondary">
-                    car <br /> types
+                    repair <br /> points
                   </div>
                 </div>
+                <button className="hidden xl:block bg-accent hover:bg-accent-hover rounded-[10px] w-full h-16 uppercase font-medium text-white tracking-[2px] text-[13px] max-w-[184px]">
+                  See all cars
+                </button>
               </div>
             </div>
           </div>
