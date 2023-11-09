76621dd9c9c0976598aed603afb1a470969e5fbc
wb-keeper <wb-keeper@mail.ru>
2023-11-09 19:26:38 +0300

    testimonials changes

--- src/app/components/TestimonialSlider.js
+++ src/app/components/TestimonialSlider.js
@@ -7,6 +7,7 @@ import "swiper/css/pagination";
 import { Pagination } from "swiper/modules";
 
 import { motion } from "framer-motion";
+import { FaQuoteLeft } from "react-icons/fa";
 
 const testimonialData = [
   {
@@ -29,7 +30,26 @@ function TestimonialSlider(props) {
     <div>
       <Swiper>
         {testimonialData.map((person, index) => {
-          return <SwiperSlide>slide 1</SwiperSlide>;
+          const { message, avatar, mane, job } = person;
+          return (
+            <SwiperSlide key={index}>
+              <div className="flex flex-col justify-center items-center text-center">
+                <FaQuoteLeft className="text-7xl text-accent mb-6" />
+                <div className="text-2xl xl:text-4xl max-w-[874px] mb-12 font-medium">
+                  {message}
+                </div>
+                <Image
+                  src={avatar}
+                  width={64}
+                  height={64}
+                  alt="avatar"
+                  className="mb-4"
+                />
+                <div className="text-lg font-medium">{name}</div>
+                <div className="text-secondary">{job}</div>
+              </div>
+            </SwiperSlide>
+          );
         })}
       </Swiper>
     </div>
