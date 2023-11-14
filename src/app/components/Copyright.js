70d9e6f7623420065094202439b3bee910e75dcc
wb-keeper <wb-keeper@mail.ru>
2023-11-14 20:55:58 +0300

    ended up with footer

--- src/app/components/Copyright.js
+++ src/app/components/Copyright.js
@@ -1,9 +1,9 @@
 const Copyright = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="text-center py-10 border-t text-sm">
+      Copyright &copy; Carland 2023. All rights reserved.
+    </div>
+  );
 };
 
 export default Copyright;
